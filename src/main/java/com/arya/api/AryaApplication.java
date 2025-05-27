5ae6273c96fdfee981226b8e58648b6a82bd048d
kevenike <kevenikepsilva@gmail.com>
2025-05-26 21:30:53 -0300

    Primeiro commit

---
+++ src/main/java/com/arya/api/AryaApplication.java
@@ -0,0 +1,13 @@
+package com.arya.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AryaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AryaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arya/api/AryaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arya.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AryaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
